5fecd6d701a5ca23d507c7cf2dae03b578d916ee
Tushar <tushar.eminenture02@gmail.com>
2026-05-16 20:36:28 +0530

    done

--- src/app.js
+++ src/app.js
@@ -13,9 +13,20 @@ const app = express();
 /*
   core middlewares
 */
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://rgrid-frontend.vercel.app",
+];
+
 app.use(
   cors({
-    origin: env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
